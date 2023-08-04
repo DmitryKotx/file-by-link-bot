94f15c79e74b1a37a3ac92c9cced841d1d4b53ef
DimaKotovProgrammist <dskotov003@gmail.com>
2023-08-04 11:49:55 +0300

    added configuration for the rabbitmq message queue.

--- dispatcher/src/main/java/ru/kotov/service/impl/UpdateProducerImpl.java
+++ dispatcher/src/main/java/ru/kotov/service/impl/UpdateProducerImpl.java
@@ -1,14 +1,22 @@
 package ru.kotov.service.impl;
 
-import lombok.extern.slf4j.Slf4j;
+import lombok.extern.log4j.Log4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.kotov.service.UpdateProducer;
-@Slf4j
+@Log4j
 @Service
 public class UpdateProducerImpl implements UpdateProducer {
+    private final RabbitTemplate rabbitTemplate;
+
+    public UpdateProducerImpl(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
     @Override
     public void produce(String rabbitQueue, Update update) {
         log.debug(update.getMessage().getText());
+        rabbitTemplate.convertAndSend(rabbitQueue, update);
     }
 }
